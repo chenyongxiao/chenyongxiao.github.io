157fb527ca1316fbde3efaea5a6a1c55be73520e
陈江 <chenjiang@ysstech.com>
2023-01-04 14:33:34 +0800

    website

---
+++ my-project/docs/src/.vuepress/config.js
@@ -0,0 +1,67 @@
+const { description } = require("../../package");
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: "奥利给",
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: "",
+    editLinks: false,
+    docsDir: "",
+    editLinkText: "",
+    lastUpdated: false,
+    nav: [
+      {
+        text: "向导",
+        link: "/guide/",
+      },
+      {
+        text: "配置",
+        link: "/config/",
+      },
+      {
+        text: "VuePress",
+        link: "https://v1.vuepress.vuejs.org",
+      },
+    ],
+    sidebar: {
+      "/guide/": [
+        {
+          title: "向导",
+          collapsable: false,
+          children: ["", "using-vue"],
+        },
+      ],
+    },
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+};
---
+++ my-project/docs/src/index.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: https://excalidraw.com/apple-touch-icon.png
+heroText: 陈永校
+tagline: 陈永校yyds
+actionText: 快速上手 →
+actionLink: https://cxkyyds.xyz
+features:
+  - title: 简洁至上
+    details: cxk yyds
+  - title: 赢时胜
+    details: 赢时胜yyds
+  - title: 前端
+    details: 前端yyds
+footer: Made by  with ❤️
+---
