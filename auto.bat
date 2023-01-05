48946c3a73d7bf69955ba97c1f971b1661fb41ed
陈江 <chenjiang@ysstech.com>
2023-01-05 10:54:00 +0800

    update

--- auto.bat
+++ auto.bat
@@ -1,9 +1,8 @@
+@echo off
 echo building...
 call yarn build
 call git add .
-@echo off
 set /p msg=commit message:
-@echo on
 call git commit -m "%msg%" 
 call git pull origin master 
 call git push origin master
